97ce4aaac252e1ed0a97a5adc2ff6eb385b1803c
tan-zhuo <tanzhuo@aliyun.com>
2026-03-29 16:30:26 +0900

    chore: bump pluginVersion to v1.6.0-beta

--- constants.go
+++ constants.go
@@ -3,7 +3,7 @@ package rabbitmq
 const (
 	// Plugin metadata
 	pluginName        = "rabbitmq"
-	pluginVersion     = "v1.5.5"
+	pluginVersion     = "v1.6.0-beta"
 	pluginDescription = "RabbitMQ message queue plugin for Lynx framework"
 	confPrefix        = "rabbitmq"
 
